3542857e98b126a14d137035027294e9d15eb400
mohamadnazimazlenn-dot <mohamadnazimazlenn@gmail.com>
2026-06-08 16:30:00 +0800

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Mengambil elemen butang berdasarkan ID
-const butang = document.getElementById('btnKlik');
-
-// Menambah fungsi interaktif apabila butang diklik
-butang.addEventListener('click', function() {
-    // Keluarkan kotak mesej lirik Liverpool
-    alert("YNWA! You'll Never Walk Alone. Terima kasih atas sokongan padu anda!");
-});
