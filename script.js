74fa15fe8a66a1837975850c4ebefd91857b0c2e
mohamadnazimazlenn-dot <mohamadnazimazlenn@gmail.com>
2026-06-08 15:11:24 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Mengambil elemen butang berdasarkan ID
+const butang = document.getElementById('btnKlik');
+
+// Menambah fungsi interaktif apabila butang diklik
+butang.addEventListener('click', function() {
+    // Keluarkan kotak mesej lirik Liverpool
+    alert("YNWA! You'll Never Walk Alone. Terima kasih atas sokongan padu anda!");
+});
